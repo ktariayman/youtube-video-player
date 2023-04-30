d785ae19e7df44b3606174a7586be680efaeea75
Aymanktari <aymanktari31@gmail.com>
2023-04-30 19:34:40 +0100

    add verification & validation to saveVideoapi

--- backend/routes/video.ts
+++ backend/routes/video.ts
@@ -6,9 +6,16 @@ import express, { Application, Request, Response } from 'express';
 import { db } from '../store/index';
 
 videoRouter.post('/', (req: Request, res: Response) => {
-  const { videoUrl, startAt, endAt, replayAt } = req.body;
+  const { videoUrl, playbackPosition } = req.body;
   if (!videoUrl) return res.status(400).send('Please provide complete information');
-  return res.send(db.saveVideo({ videoUrl, startAt, endAt, replayAt }));
+
+  const videoExist = db.getVideo(videoUrl);
+
+  if (!videoExist) {
+    return res.send(db.saveVideo({ videoUrl, playbackPosition }));
+  } else {
+    return res.send(db.updateVideo({ videoUrl, playbackPosition }));
+  }
 });
 
 videoRouter.get('/', (req: Request, res: Response) => {
@@ -21,6 +28,10 @@ videoRouter.get('/:videoUrl', (req: Request, res: Response) => {
     return res.status(400).send('Please provide a video URL');
   }
   return res.status(200).send(db.getVideo(videoUrl));
+});
+
+videoRouter.put('/:videoUrl', (req: Request, res: Response) => {
+  return res.status(200).send(db.getVideos());
 });
 
 export default videoRouter;
