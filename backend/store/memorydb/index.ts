db937a8f97454d068f5a22afeb8332335b43925e
Aymanktari <aymanktari31@gmail.com>
2023-04-30 19:34:49 +0100

    add updatevideo

--- backend/store/memorydb/index.ts
+++ backend/store/memorydb/index.ts
@@ -5,10 +5,17 @@ export class InMemoryDatastore implements Datastore {
   saveVideo(video: Video): void {
     this.videos.push(video);
   }
+
   getVideo(url: string): Video | undefined {
     return this.videos.find((v) => v.videoUrl === url);
   }
   getVideos(): Video[] | undefined {
     return this.videos;
+  }
+  updateVideo(video: Video): void {
+    const index = this.videos.findIndex((v) => v.videoUrl === video.videoUrl);
+    if (index !== -1) {
+      this.videos[index] = video;
+    }
   }
 }
